8cf0f8a24d14671973d437e55c30598d0582c215
Tima <74903776+aarystanovv@users.noreply.github.com>
2022-12-27 22:40:05 +0600

    Add files via upload

---
+++ HW5.php
@@ -0,0 +1,65 @@
+<?php
+date_default_timezone_set("Etc/GMT-6");
+echo date_default_timezone_get();
+echo "<br>";echo "<br>";echo "<br>";
+
+
+// 1 Zadanie
+
+function data(){
+    $day = date("d.m.y");
+    echo $day;
+    echo "<br>";echo "<br>";echo "<br>";
+}
+
+data();
+
+
+// 2 Zadanie
+ function data2($data){
+     echo "<h2>Время в Алмате: $data</h2>";
+ }
+
+ $data = date("d.m.Y H:i:s");
+ data2($data);
+
+// 3 Zadanie
+function proverka($int){
+    if($int % 2 == 0){
+        echo "Число $int является четной и поэтому: true";
+        echo "<br>";
+    }else{
+        echo "Число $int является нечетной и поэтому: false";
+        echo "<br>";
+    }
+}
+
+proverka(7);
+proverka(12);
+
+// 4 Zadanie
+function vote($age){
+    if($age >= 18){
+        echo "Вы взрослый, можете голосовать";
+        echo "<br>";
+    }elseif ($age < 18){
+        $limitation = 18 - $age;
+        echo "Чтобы можно было голосовать осталось $limitation года (лет)";
+        echo "<br>";
+    }
+}
+
+vote(14);
+vote(21);
+
+// 5 Zadanie
+function test(...$intValues)
+{
+    $result = '';
+    foreach ($intValues as $intValue) {
+        $result .= $intValue;
+        return max(5, 8, 11, 0,  4, 3, 9, 1);
+    }
+    return $result;
+}
+echo test(5, 8, 11, 0,  4, 3, 9, 1);
