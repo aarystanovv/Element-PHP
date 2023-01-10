9a93bdfc35f49c7d256e3cb45007207eeeb7242e
Tima <74903776+aarystanovv@users.noreply.github.com>
2023-01-10 14:53:36 +0600

    Create Car.php

---
+++ HW6/Car.php
@@ -0,0 +1,52 @@
+<?php
+use Driver;
+use Engine;
+require_once $_SERVER['DOCUMENT_ROOT']. '/Extra/Helpers.php';
+
+class Car {
+    public String $marka;
+    public String $carClass;
+    public float $weight;
+    public Driver $driver;
+    public Engine $engine;
+
+    public function __construct($marka, $carClass, $weight, $driver, $engine)
+    {
+        $this->marka = $marka;
+        $this->carClass = $carClass;
+        $this->weight = $weight;
+        $this->driver = $driver;
+        $this->engine = $engine;
+    }
+
+    public function start(){
+        pre("Поехали");
+    }
+
+    public function stop(){
+        pre("Останавливаемся");
+    }
+
+    public function turnRight(){
+        pre("Поворот направо");
+    }
+
+    public function turnLeft(){
+        pre("Поворот налево");
+    }
+
+}
+
+class Lorry extends Car{
+    public int $carrying;
+}
+
+class SportCar extends Car{
+    public float $speed;
+}
+
+$obj = new Car();
+
+$obj->marka = 'BMW X5';
+$obj->weight = 2860;
+$obj->carClass = 'crossover';
