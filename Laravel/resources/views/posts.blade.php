947769ba011893826c68edc33b8638f77bdaaf03
Tima <74903776+aarystanovv@users.noreply.github.com>
2023-02-22 00:40:22 +0600

    Add files via upload

---
+++ Laravel/resources/views/posts.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="/vendor/bootstrap-5.3.0-alpha1-dist/css/bootstrap.css">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Posts</title>
+</head>
+<body>
+<div class="container">
+    <h1>Posts</h1>
+
+    <article>
+        <h2><a href="/post/post1">Post 1</a></h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, repellendus.</p>
+    </article>
+
+    <article>
+        <h2><a href="/post/post2">Post 2</a></h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, repellendus.</p>
+    </article>
+
+    <article>
+        <h2><a href="/post/post3">Post 3</a></h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, repellendus.</p>
+    </article>
+</div>
+</body>
+</html>
